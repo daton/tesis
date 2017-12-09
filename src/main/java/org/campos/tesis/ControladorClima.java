73718c7cdf6cdda9aebdbc8a777457309be42411
como-quesito <celiesita1951>
2017-12-08 18:13:20 -0600

    aaaa

--- src/main/java/org/campos/tesis/ControladorClima.java
+++ src/main/java/org/campos/tesis/ControladorClima.java
@@ -24,7 +24,7 @@ public class ControladorClima {
         System.out.println("gaurdado con exito");
         Estatus e=new Estatus();
         e.setSuccess(true);
-        e.setMensaje("Usuario se ha guardado con exito");
+        e.setMensaje("Estacion guardada copn exito");
         return e;
 
     }
